1b7a0d985745f0189438f3e2755a3ee416c5a806
Loïc Pottier <loic.pottier@live.fr>
2025-05-05 16:29:34 -0700

    feat(ecs): add built-in transform component

---
+++ crates/ecs/src/components/transform.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone)]
+pub struct Transform {
+  pub position: [f32; 3],
+  pub rotation: [f32; 3],
+  pub scale: [f32; 3],
+}
+
+impl Default for Transform {
+  fn default() -> Self {
+    Self {
+      position: [0.0, 0.0, 0.0],
+      rotation: [0.0, 0.0, 0.0],
+      scale: [1.0, 1.0, 1.0],
+    }
+  }
+}
