cb30a3bf776f88848a5b0f6fbdf8b7b2788e0dfe
Loïc Pottier <loic.pottier@live.fr>
2025-05-05 15:43:16 -0700

    feat(ecs): add component trait
    add imports to lib.rs file

---
+++ crates/ecs/src/component.rs
@@ -0,0 +1,52 @@
+use crate::entity::Entity;
+use std::collections::HashMap;
+use std::any::Any;
+
+pub trait Component: 'static + Send + Sync {}
+impl<T: 'static + Send + Sync> Component for T {}
+
+pub trait ComponentStorage {
+  fn remove(&mut self, entity: Entity);
+  fn as_any(&self) -> &dyn Any;
+  fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+pub struct DenseComponentStorage<T> {
+  data: HashMap<u32, T> // Entity.id -> Component
+}
+
+impl<T> Default for DenseComponentStorage<T> {
+  fn default() -> Self {
+    Self {
+      data: HashMap::new(),
+    }
+  }
+}
+
+impl<T> DenseComponentStorage<T> {
+  pub fn insert(&mut self, entity: Entity, component: T) {
+    self.data.insert(entity.id, component);
+  }
+
+  pub fn get(&self, entity: Entity) -> Option<&T> {
+    self.data.get(&entity.id)
+  }
+
+  pub fn get_mut(&mut self, entity: Entity) -> Option<&mut T> {
+    self.data.get_mut(&entity.id)
+  }
+}
+
+impl<T: 'static> ComponentStorage for DenseComponentStorage<T> {
+  fn remove(&mut self, entity: Entity) {
+    self.data.remove(&entity.id);
+  }
+
+  fn as_any(&self) -> &dyn Any {
+    self
+  }
+
+  fn as_any_mut(&mut self) -> &mut dyn Any {
+    self
+  }
+}
---
+++ crates/ecs/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod entity;
+pub mod component;
+pub mod world;
+pub mod system;
+
