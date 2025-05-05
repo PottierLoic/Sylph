c94e9919f84103ec1a9cd5612f83cbae944be442
Loïc Pottier <loic.pottier@live.fr>
2025-05-05 15:41:22 -0700

    feat(ecs): add entity struct

---
+++ crates/ecs/src/entity.rs
@@ -0,0 +1,53 @@
+use std::collections::VecDeque;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Entity {
+  pub id: u32,
+  pub generation: u32,
+}
+
+#[derive(Default)]
+pub struct EntityManager {
+  generations: Vec<u32>,
+  free: VecDeque<u32>,
+}
+
+impl EntityManager {
+  pub fn create(&mut self) -> Entity {
+    if let Some(id) = self.free.pop_front() {
+      let generations = self.generations[id as usize];
+      Entity { id, generation: generations }
+    } else {
+      let id = self.generations.len() as u32;
+      self.generations.push(0);
+      Entity { id, generation: 0 }
+    }
+  }
+
+  pub fn destroy(&mut self, entity: Entity) {
+    let id = entity.id as usize;
+    if self.is_alive(entity) {
+      self.generations[id] += 1;
+      self.free.push_back(entity.id);
+    }
+  }
+
+  pub fn is_alive(&self, entity: Entity) -> bool {
+    self.generations
+      .get(entity.id as usize)
+      .is_some_and(|&generation| generation == entity.generation)
+  }
+
+  pub fn alive_iter(&self) -> impl Iterator<Item = Entity> + '_ {
+    self.generations
+      .iter()
+      .enumerate()
+      .filter_map(|(id, &generation)| {
+        if !self.free.contains(&(id as u32)) {
+          Some(Entity { id: id as u32, generation })
+        } else {
+          None
+        }
+      })
+  }
+}
